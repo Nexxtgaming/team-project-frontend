6966cd84dd8cd6c786ce9d962802479004a672b9
Auraen <aurane.martin@etu.univ-tours.fr>
2022-05-30 23:47:40 +0200

    fixed carousel

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,26 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
-	selector: 'app-carousel',
-	templateUrl: './carousel.component.html',
-	styleUrls: ['./carousel.component.css'],
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
-	message: string;
-
-  availableMessage: string[];
-
-	constructor() {
-    this.availableMessage = ["Who are we?","What is this?","What's different?"];
-    this.message = this.availableMessage[0]
-  }
-
-	ngOnInit(): void {}
-
-  next(): void {
-    let index = this.availableMessage.indexOf(this.message,0);
-    index = (index + 1) % this.availableMessage.length;
-    this.message = this.availableMessage[index]
-
+  ngOnInit():
+    void {
   }
 }

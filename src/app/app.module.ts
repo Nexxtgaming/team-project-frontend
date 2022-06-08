1c5ae66828a79b5d1c8d78719f809958d36987af
Auraen <aurane.martin@etu.univ-tours.fr>
2022-06-08 23:27:05 +0200

    test upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { CarouselComponent } from './home/carousel/carousel.component';
-import { InformationComponent } from './home/information/information.component';
-import { FooterComponent } from './footer/footer.component';
-import { LinksComponent } from './home/information/links/links.component';
-import { TwitterFeedComponent } from './home/information/twitter-feed/twitter-feed.component';
-import { LogoComponent } from './footer/logo/logo.component';
-import { SuggestionsComponent } from './suggestions/suggestions.component';
-import { MapOffersComponent } from './map-offers/map-offers.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {CarouselComponent} from './home/carousel/carousel.component';
+import {InformationComponent} from './home/information/information.component';
+import {FooterComponent} from './footer/footer.component';
+import {LinksComponent} from './home/information/links/links.component';
+import {TwitterFeedComponent} from './home/information/twitter-feed/twitter-feed.component';
+import {LogoComponent} from './footer/logo/logo.component';
+import {SuggestionsComponent} from './suggestions/suggestions.component';
+import {MapOffersComponent} from './map-offers/map-offers.component';
+import {GoogleMapsModule} from "@angular/google-maps";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		HomeComponent,
-		CarouselComponent,
-		InformationComponent,
-		FooterComponent,
-  LinksComponent,
-  TwitterFeedComponent,
-  LogoComponent,
-  SuggestionsComponent,
-  MapOffersComponent,
-	],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CarouselComponent,
+    InformationComponent,
+    FooterComponent,
+    LinksComponent,
+    TwitterFeedComponent,
+    LogoComponent,
+    SuggestionsComponent,
+    MapOffersComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
